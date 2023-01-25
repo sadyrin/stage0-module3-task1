b1ca2b9b876166137574826155a019421d97127a
sadyrin <sadyrin@gmail.com>
2023-01-25 19:01:53 +0200

    Done tasks "Module #3. Task #1. Syntax"

--- src/main/java/lang/print/gaps/task1/PrintWordsApp.java
+++ src/main/java/lang/print/gaps/task1/PrintWordsApp.java
@@ -2,6 +2,14 @@ package lang.print.gaps.task1;
 
 public class PrintWordsApp {
     public static void main(String[] args) {
-        //write your code here
+        String newLineCharacter = "\n";
+
+        String[] words = {"java", "is", "smth"};
+
+        for(int i = 0; i < words.length; i++){
+            System.out.print(words[i]);
+
+            if(i != words.length - 1) System.out.print(newLineCharacter);
+        }
     }
 }
